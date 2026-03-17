d2a1a7d47434641f8f94297b4a3bc70bc0279906
Sadaka-Wijerathna <buddysellertg1@gmail.com>
2026-03-18 04:49:44 +0530

    Fix: Remove prisma.config.ts to resolve failed deployment builds

--- prisma.config.ts
+++ prisma.config.ts
@@ -21,18 +21,11 @@
  */
 import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
-import { PrismaNeonHttp } from '@prisma/adapter-neon';
 
 export default defineConfig({
   earlyAccess: true,
   schema: 'prisma/schema.prisma',
   datasource: {
     url: process.env.DATABASE_URL!,
-  },
-  migrate: {
-    async adapter() {
-      const connectionString = process.env.DATABASE_URL!;
-      return new PrismaNeonHttp(connectionString, {});
-    },
-  },
+  }
 });
